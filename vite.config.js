da86a56b8b723383bfd5d93a71b739eab90a97e0
manoj-hubgit <manojannadurai2265@gmail.com>
2024-07-06 07:51:22 +0530

    file changed

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{port:3000}
-})
+},
+{ build: {
+  rollupOptions: {
+    external: ['chart.js'],
+  },
+}}
+)
