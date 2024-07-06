373fef1f715631bdd5e223db4350ffdab50b7dfa
manoj-hubgit <manojannadurai2265@gmail.com>
2024-07-06 08:27:39 +0530

    added

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:3000}
-},
-)
+  server: {
+    port: 3000
+  },
+  build: {
+    rollupOptions: {
+      external: ['bootstrap/dist/css/bootstrap.min.css']
+    }
+  }
+});
